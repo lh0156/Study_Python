767a7017276f152eec61f07e02aa0610d4f730ee
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-08-09 22:13:40 +0900

    add list and Dictionary

---
+++ listAndDictionary.py
@@ -0,0 +1,14 @@
+carList = ['K5', 'white', '5000']
+
+print(carList)
+print(carList[0])
+
+carList[1] = 'black'
+
+print(carList)
+
+carList.reverse()
+print(carList)
+
+carList2 = { 'brand' : 'BMW', 'model' : '520d'}
+print(carList2)
